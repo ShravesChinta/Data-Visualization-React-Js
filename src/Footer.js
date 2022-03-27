12d31f9e03ed9887f3b6bb1377c0c43b7619351f
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-03-27 09:11:27 -0600

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+  
+  <section class="footor bg-dark text-white">
+    <br/>
+    <p class="lead container"><a href="https://github.com/ShravesChinta/Data_Visualization_Project.git">Github Repo</a></p>
+    <p class="lead container"><a href="https://www.linkedin.com/in/sravanti-chinta-432390110/">Linkedin</a></p>
+    <br/>
+  </section>
+);
+
+export default Footer;
